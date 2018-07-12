33c734ca97fb9c8a056e66fdbd66b02935dceb91
Miguel Martin <pomikelor@gmail.com>
2018-07-12 20:24:00 +0200

    Añado menu desplegable

--- js/main.js
+++ js/main.js
@@ -1,21 +1,29 @@
 export class Main{
     constructor(){
         //Elementos del DOM
+        this.iconoMenu = document.querySelector('.fa-bars')
+        this.itemMenu = document.querySelector('#lista-menu')
         this.formContact = document.querySelector('#contacto')
-        //this.iconoMenu = document.querySelector('#iconoMenu')
+        
         this.menu = document
         this.secciones =
         this.offsets = []
     }
 
     defineEventListener(){
-        this.formContact.addEventListener('submit', );
-        this.iconoMenu.addEventListener('click', this.toggleMenu)
+        /* this.formContact.addEventListener('submit', ); */
+        this.iconoMenu.addEventListener('click', this.addMenu)
+        this.itemMenu.addEventListener('click', this.removeMenu)
         window.addEventListener('scroll', this.changeMenuStyle.bind(this))
     }
 
-    toggleMenu(){
-        //document.querySelector('#menutop').classList.toggle('hide')
+    addMenu(){
+        
+        document.querySelector('.menu-top').classList.add('hidden');
+    }
+    removeMenu(e){
+        console.log(e);
+        document.querySelector('.menu-top').classList.remove('hidden');
     }
 
     changeMenuStyle(e){
