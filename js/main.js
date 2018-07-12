a270fb5044ac5d8d5dd73429832d350d2da31649
Miguel Martin <pomikelor@gmail.com>
2018-07-12 08:51:54 +0200

    aplico media query

---
+++ js/main.js
@@ -0,0 +1,28 @@
+export class Main{
+    constructor(){
+        //Elementos del DOM
+        this.formContact = document.querySelector('#contacto')
+        //this.iconoMenu = document.querySelector('#iconoMenu')
+        this.menu = document
+        this.secciones =
+        this.offsets = []
+    }
+
+    defineEventListener(){
+        this.formContact.addEventListener('submit', );
+        this.iconoMenu.addEventListener('click', this.toggleMenu)
+        window.addEventListener('scroll', this.changeMenuStyle.bind(this))
+    }
+
+    toggleMenu(){
+        //document.querySelector('#menutop').classList.toggle('hide')
+    }
+
+    changeMenuStyle(e){
+
+
+
+    }
+
+
+}
