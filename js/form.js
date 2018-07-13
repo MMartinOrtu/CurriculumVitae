5269fbb9de6e1b4b1aa57217240f971cc205d72e
Miguel Martin <pomikelor@gmail.com>
2018-07-13 14:53:51 +0200

    Añado Js nav y form

--- js/form.js
+++ js/form.js
@@ -1,5 +1,28 @@
 export class Form {
     constructor(){
+        //Elemento del DOM del formulario
+        this.name= document.querySelector('#name');
+        this.email= document.querySelector('#email');
+        this.phone= document.querySelector('#phone');
+        this.opciones= document.querySelectorAll('#opciones');
+        this.otros= document.querySelector('#opcion-otros');
+        this.message= document.querySelector('#message');
+        this.btnForm= document.querySelector('btnForm');
         
+        this.datosForm ={
+            name: '',
+            email:'',
+            phone:'',
+            conocido: '',
+            otros:'',
+            message:''
+        }
+        
+       
     }
+   
+    guardarDatos(){
+        console.log('guardando datos')
+    }
+
 }
--- js/main.js
+++ js/main.js
@@ -1,20 +1,30 @@
+import {Form} from "./form.js"
+
 export class Main{
     constructor(){
         //Elementos del DOM
-        this.iconoMenu = document.querySelector('.fa-bars')
-        this.itemMenu = document.querySelector('#lista-menu')
-        this.formContact = document.querySelector('#contacto')
+        this.iconoMenu = document.querySelector('.fa-bars');
+        this.itemMenu = document.querySelector('#lista-menu');
+        this.formContact = document.querySelector('#contacto');
+        this.menu = document.querySelectorAll('nav.menu-top a');
+        this.secciones = document.querySelectorAll('.secciones');
         
-        this.menu = document
-        this.secciones =
-        this.offsets = []
+        this.offsets = [];
+        this.prepararNavegacion();
+
+        this.opciones= document.querySelector('#listado');
+        this.otros= document.querySelector('#otros');
+     
     }
 
     defineEventListener(){
-        /* this.formContact.addEventListener('submit', ); */
-        this.iconoMenu.addEventListener('click', this.addMenu)
-        this.itemMenu.addEventListener('click', this.removeMenu)
-        window.addEventListener('scroll', this.changeMenuStyle.bind(this))
+        this.formContact.addEventListener('submit', this.readContact);
+        this.iconoMenu.addEventListener('click', this.addMenu);
+       /*  this.iconoMenu.addEventListener('touch', this.addMenu); */
+        this.itemMenu.addEventListener('click', this.removeMenu);
+        this.opciones.addEventListener('change', this.selectOption.bind(this));
+        window.addEventListener('scroll', this.changeMenuStyle.bind(this));
+
     }
 
     addMenu(){
@@ -26,11 +36,46 @@ export class Main{
         document.querySelector('.menu-top').classList.remove('hidden');
     }
 
-    changeMenuStyle(e){
-
-
-
+   changeMenuStyle(){       
+     let pageOffset = window.pageYOffset;
+     let menuItem = 0    
+    
+        if(pageOffset>=this.offsets['#sobre-mi'] && pageOffset<this.offsets['#formacion']){
+            menuItem= 0;            
+        }else if(pageOffset>=this.offsets['#formacion'] && pageOffset<this.offsets['#experiencia']){
+            menuItem= 1;    
+        }else if(pageOffset>=this.offsets['#experiencia'] && pageOffset<this.offsets['#contacto']){
+            menuItem= 2;
+        }else if(pageOffset>=this.offsets['#contacto']){
+            menuItem= 3;
+        }
+        
+        this.menu.forEach(
+            (item) => item.classList.remove('active')
+        )
+        this.menu[menuItem].classList.add('active')       
     }
 
+    prepararNavegacion() {         
+         this.secciones.forEach(
+            (item) => {
+                let sectionoffSet =item.offsetTop;                
+                this.offsets['#'+item.id] = sectionoffSet;
+            }
+        ) 
+    }
+
+    readContact(e){
+        e.preventDefault();
+        let form = new Form();
+        form.guardarDatos();
+
+    }
+    selectOption(e){
+        console.log(this.otros)
+        if(e.target.value=="Otros"){
+            this.otros.classList.remove('input-hidden')
+        }
+    }
 
 }
