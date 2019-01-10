49416b015e2b82aae5ff52f78b69a72b39a7c437
Miguel Martín <pomikelor@gmail.com>
2019-01-10 13:48:43 +0100

    get

--- sendEmail.php
+++ sendEmail.php
@@ -1,16 +1,16 @@
 <?php
-if(isset($_POST['submit'])) {
+if(isset($_GET['submit'])) {
 
   // Debes editar las próximas dos líneas de código de acuerdo con tus preferencias
   $email_to = "pomikelor@gmail.com";
   $email_subject = "Contacto desde el sitio web";
 
   // Aquí se deberían validar los datos ingresados por el usuario
-  if(!isset($_POST['name']) ||
-  !isset($_POST['email']) ||
-  !isset($_POST['phone']) ||
-  !isset($_POST['opcion-otros']) ||
-  !isset($_POST['message'])) {
+  if(!isset($_GET['name']) ||
+  !isset($_GET['email']) ||
+  !isset($_GET['phone']) ||
+  !isset($_GET['opcion-otros']) ||
+  !isset($_PGE['message'])) {
 
   echo "<b>Ocurrió un error y el formulario no ha sido enviado. </b><br />";
   echo "Por favor, vuelva atrás y verifique la información ingresada<br />";
@@ -18,11 +18,11 @@ if(isset($_POST['submit'])) {
 }
 
 $email_message = "Detalles del formulario de contacto:\n\n";
-$email_message .= "Nombre: " . $_POST['name'] . "\n";
-$email_message .= "Apellido: " . $_POST['email'] . "\n";
-$email_message .= "E-mail: " . $_POST['phone'] . "\n";
-$email_message .= "Teléfono: " . $_POST['opcion-otros'] . "\n";
-$email_message .= "Comentarios: " . $_POST['message'] . "\n\n";
+$email_message .= "Nombre: " . $_GET['name'] . "\n";
+$email_message .= "Apellido: " . $_GET['email'] . "\n";
+$email_message .= "E-mail: " . $_GET['phone'] . "\n";
+$email_message .= "Teléfono: " . $_GET['opcion-otros'] . "\n";
+$email_message .= "Comentarios: " . $_GET['message'] . "\n\n";
 
 
 // Ahora se envía el e-mail usando la función mail() de PHP
